f103ef2a34d1d5dd49cfe5ca08209900fd3b3c96
Vishal Sharma <vsh1819@gmail.com>
2018-07-12 18:24:32 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,6 @@ class ApplicationController < Sinatra::Base
     end
 
 		if params[:username] != "" || params[:password] != ""
-		    User.new(:username => params[:username], :password => params[:password])
         redirect "/login"
     end
     
