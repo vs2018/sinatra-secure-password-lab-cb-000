bbaf8bfb360f4d31c963ae98e8d7cb327ce97e20
Vishal Sharma <vsh1819@gmail.com>
2018-07-12 18:22:21 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,7 @@ class ApplicationController < Sinatra::Base
     username = params[:username]
   
     
-    if user.class != User
+    if username == "Username:"
         redirect "/failure"
     end
 
