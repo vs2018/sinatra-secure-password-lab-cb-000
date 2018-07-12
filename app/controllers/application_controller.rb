240953e203c029e212f5612089994ca40354cc1c
Vishal Sharma <vsh1819@gmail.com>
2018-07-12 18:18:44 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < Sinatra::Base
         redirect "/login"
     end
     
-    if params[:username] == "" || params[:password] == ""
+    if params[:username] == nil || params[:password] == nil
         redirect "/failure"
     end
 
